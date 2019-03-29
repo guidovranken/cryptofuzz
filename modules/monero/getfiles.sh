936c69cf3a49fe8ba5b9d513060d340ff475312d
Guido Vranken <guidovranken@gmail.com>
2019-03-29 11:18:16 +0100

    Monero: remove old files before downloading new in getfiles.sh

--- modules/monero/getfiles.sh
+++ modules/monero/getfiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/monero-project/monero/master/contrib/epee/include/int-util.h
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/groestl.c
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/groestl.h
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/groestl_tables.h
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/jh.c
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/jh.h
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/skein.c
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/skein.h
-wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/skein_port.h
+rm -f int-util.h; wget https://raw.githubusercontent.com/monero-project/monero/master/contrib/epee/include/int-util.h
+rm -f groestl.c; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/groestl.c
+rm -f groestl.h; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/groestl.h
+rm -f groestl_tables.h; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/groestl_tables.h
+rm -f jh.c; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/jh.c
+rm -f jh.h; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/jh.h
+rm -f skein.c; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/skein.c
+rm -f skein.h; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/skein.h
+rm -f skein_port.h; wget https://raw.githubusercontent.com/monero-project/monero/master/src/crypto/skein_port.h
