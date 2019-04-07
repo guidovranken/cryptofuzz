fe4c9235493537d194f6940d720f4b6b113f102a
Guido Vranken <guidovranken@gmail.com>
2019-04-07 23:33:30 +0200

    Monero: Add Skein, Keccak, Keccak HMAC

--- modules/monero/module.cpp
+++ modules/monero/module.cpp
@@ -5,6 +5,8 @@
 extern "C" {
 #include "groestl.h"
 #include "jh.h"
+#include "skein.h"
+#include "hmac-keccak.h"
 }
 
 namespace cryptofuzz {
@@ -31,6 +33,39 @@ std::optional<component::Digest> Monero::jh(operation::Digest& op, Datasource& d
     jh_hash(hashSize, op.cleartext.GetPtr(), op.cleartext.GetSize() * 8, out);
 
     return component::Digest(out, hashSize / 8);
+}
+
+std::optional<component::Digest> Monero::skein(operation::Digest& op, Datasource& ds, const size_t hashSize) const {
+    (void)ds;
+
+    unsigned char out[hashSize / 8];
+
+    skein_hash(hashSize, op.cleartext.GetPtr(), op.cleartext.GetSize() * 8, out);
+
+    return component::Digest(out, hashSize / 8);
+}
+
+std::optional<component::Digest> Monero::keccak256(operation::Digest& op, Datasource& ds) const {
+    KECCAK_CTX ctx;
+    util::Multipart parts;
+
+    /* Initialize */
+    {
+        parts = util::ToParts(ds, op.cleartext);
+        keccak_init(&ctx);
+    }
+
+    /* Process */
+    for (const auto& part : parts) {
+        keccak_update(&ctx, part.first, part.second);
+    }
+
+    /* Finalize */
+    {
+        uint8_t out[32];
+        keccak_finish(&ctx, out);
+        return component::Digest(out, sizeof(out));
+    }
 }
 
 std::optional<component::Digest> Monero::OpDigest(operation::Digest& op) {
@@ -63,6 +98,62 @@ std::optional<component::Digest> Monero::OpDigest(operation::Digest& op) {
         case ID("Cryptofuzz/Digest/JH-512"):
             {
                 return jh(op, ds, 512);
+            }
+            break;
+        case ID("Cryptofuzz/Digest/SKEIN-256"):
+            {
+                return skein(op, ds, 256);
+            }
+            break;
+        case ID("Cryptofuzz/Digest/SKEIN-512"):
+            {
+                return skein(op, ds, 512);
+            }
+            break;
+        case ID("Cryptofuzz/Digest/SKEIN-1024"):
+            {
+                return skein(op, ds, 1024);
+            }
+            break;
+        case ID("Cryptofuzz/Digest/KECCAK-256"):
+            {
+                return keccak256(op, ds);
+            }
+            break;
+    }
+
+    return ret;
+}
+
+std::optional<component::MAC> Monero::OpHMAC(operation::HMAC& op) {
+    using fuzzing::datasource::ID;
+
+    Datasource ds(op.modifier.GetPtr(), op.modifier.GetSize());
+    std::optional<component::MAC> ret = std::nullopt;
+
+    switch ( op.digestType.Get() ) {
+        case ID("Cryptofuzz/Digest/KECCAK-256"):
+            {
+                auto parts = util::ToParts(ds, op.cleartext);
+
+                hmac_keccak_state S;
+
+                /* Initialize */
+                {
+                    hmac_keccak_init(&S, op.cipher.key.GetPtr(), op.cipher.key.GetSize());
+                }
+
+                /* Process */
+                for (const auto& part : parts) {
+                    hmac_keccak_update(&S, part.first, part.second);
+                }
+
+                /* Finalize */
+                {
+                    uint8_t out[32];
+                    hmac_keccak_finish(&S, out);
+                    ret = component::Digest(out, sizeof(out));
+                }
             }
             break;
     }
--- modules/monero/module.h
+++ modules/monero/module.h
@@ -11,9 +11,12 @@ class Monero : public Module {
     private:
         std::optional<component::Digest> groestl(operation::Digest& op, Datasource& ds) const;
         std::optional<component::Digest> jh(operation::Digest& op, Datasource& ds, const size_t hashSize) const;
+        std::optional<component::Digest> skein(operation::Digest& op, Datasource& ds, const size_t hashSize) const;
+        std::optional<component::Digest> keccak256(operation::Digest& op, Datasource& ds) const;
     public:
         Monero(void);
         std::optional<component::Digest> OpDigest(operation::Digest& op) override;
+        std::optional<component::MAC> OpHMAC(operation::HMAC& op) override;
 };
 
 } /* namespace module */
