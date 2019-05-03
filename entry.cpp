481db902b247fd039d4589cf88e5b5bd654b1a8d
Guido Vranken <guidovranken@gmail.com>
2019-05-03 23:10:27 +0200

    Better support for multiple modules in build procedure

--- entry.cpp
+++ entry.cpp
@@ -4,11 +4,34 @@
 #include "driver.h"
 
 #include <modules/openssl/module.h>
-//#include <modules/publicdomain/module.h>
-//#include <modules/cppcrypto/module.h>
-//#include <modules/mbedtls/module.h>
-//#include <modules/boost/module.h>
-//#include <modules/monero/module.h>
+
+#if defined(CRYPTOFUZZ_REFERENCE)
+  #include <modules/reference/module.h>
+#endif
+
+#if defined(CRYPTOFUZZ_CPPCRYPTO)
+  #include <modules/cppcrypto/module.h>
+#endif
+
+#if defined(CRYPTOFUZZ_MBEDTLS)
+  #include <modules/mbedtls/module.h>
+#endif
+
+#if defined(CRYPTOFUZZ_BOOST)
+  #include <modules/boost/module.h>
+#endif
+
+#if defined(CRYPTOFUZZ_MONERO)
+  #include <modules/monero/module.h>
+#endif
+
+#if defined(CRYPTOFUZZ_VERACRYPT)
+  #include <modules/veracrypt/module.h>
+#endif
+
+#if defined(CRYPTOFUZZ_LIBSODIUM)
+  #include <modules/libsodium/module.h>
+#endif
 
 std::shared_ptr<cryptofuzz::Driver> driver = nullptr;
 
@@ -19,11 +42,34 @@ extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
     driver = std::make_shared<cryptofuzz::Driver>();
 
     driver->LoadModule( std::make_shared<cryptofuzz::module::OpenSSL>() );
-    //driver->LoadModule( std::make_shared<cryptofuzz::module::mbedTLS>() );
-    //driver->LoadModule( std::make_shared<cryptofuzz::module::Boost>() );
-    //driver->LoadModule( std::make_shared<cryptofuzz::module::PublicDomain>() );
-    //driver->LoadModule( std::make_shared<cryptofuzz::module::CPPCrypto>() );
-    //driver->LoadModule( std::make_shared<cryptofuzz::module::Monero>() );
+
+#if defined(CRYPTOFUZZ_REFERENCE)
+    driver->LoadModule( std::make_shared<cryptofuzz::module::Reference>() );
+#endif
+
+#if defined(CRYPTOFUZZ_CPPCRYPTO)
+    driver->LoadModule( std::make_shared<cryptofuzz::module::CPPCrypto>() );
+#endif
+
+#if defined(CRYPTOFUZZ_MBEDTLS)
+    driver->LoadModule( std::make_shared<cryptofuzz::module::mbedTLS>() );
+#endif
+
+#if defined(CRYPTOFUZZ_BOOST)
+    driver->LoadModule( std::make_shared<cryptofuzz::module::Boost>() );
+#endif
+
+#if defined(CRYPTOFUZZ_MONERO)
+    driver->LoadModule( std::make_shared<cryptofuzz::module::Monero>() );
+#endif
+
+#if defined(CRYPTOFUZZ_VERACRYPT)
+    driver->LoadModule( std::make_shared<cryptofuzz::module::Veracrypt>() );
+#endif
+
+#if defined(CRYPTOFUZZ_LIBSODIUM)
+    driver->LoadModule( std::make_shared<cryptofuzz::module::libsodium>() );
+#endif
 
     return 0;
 }
