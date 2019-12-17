c3322b87f17818a4a9ba587823dd7ae9a79023c7
Guido Vranken <guidovranken@gmail.com>
2019-12-17 04:50:39 +0100

    mbedTLS module: Support PKCS12 PBKDF

--- modules/mbedtls/module.h
+++ modules/mbedtls/module.h
@@ -16,6 +16,7 @@ class mbedTLS : public Module {
         std::optional<component::Ciphertext> OpSymmetricEncrypt(operation::SymmetricEncrypt& op) override;
         std::optional<component::Cleartext> OpSymmetricDecrypt(operation::SymmetricDecrypt& op) override;
         std::optional<component::Key> OpKDF_HKDF(operation::KDF_HKDF& op) override;
+        std::optional<component::Key> OpKDF_PBKDF(operation::KDF_PBKDF& op) override;
         std::optional<component::Key> OpKDF_PBKDF2(operation::KDF_PBKDF2& op) override;
         std::optional<component::ECC_PublicKey> OpECC_PrivateToPublic(operation::ECC_PrivateToPublic& op) override;
         std::optional<bool> OpECDSA_Verify(operation::ECDSA_Verify& op) override;
