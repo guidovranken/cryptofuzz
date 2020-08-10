86eb90a96a34a42eed93f5aa3cd31afa2c8caf3d
Guido Vranken <guidovranken@gmail.com>
2020-08-10 22:18:32 +0200

    wolfCrypt: Temporarily disable ECC_PrivateToPublic (ZD 10749)

--- modules/wolfcrypt/module.cpp
+++ modules/wolfcrypt/module.cpp
@@ -1982,6 +1982,9 @@ namespace wolfCrypt_detail {
 
 std::optional<component::ECC_PublicKey> wolfCrypt::OpECC_PrivateToPublic(operation::ECC_PrivateToPublic& op) {
     std::optional<component::ECC_PublicKey> ret = std::nullopt;
+    /* Disabled until ZD 10749 is resolved */
+    (void)op;
+#if 0
     Datasource ds(op.modifier.GetPtr(), op.modifier.GetSize());
     wolfCrypt_detail::SetGlobalDs(&ds);
 
@@ -2025,6 +2028,7 @@ end:
 
     wolfCrypt_detail::UnsetGlobalDs();
 
+#endif
     return ret;
 }
 
