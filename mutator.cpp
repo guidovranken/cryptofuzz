57359b42bf15598dcdae97d2e5048f06e72bf2f2
Guido Vranken <guidovranken@gmail.com>
2020-09-17 20:08:35 +0200

    Improve mutator efficiency

--- mutator.cpp
+++ mutator.cpp
@@ -14,14 +14,31 @@ static uint32_t PRNG()
     return nSeed  % 32767;
 }
 
+static std::vector<size_t> SplitLength(size_t left, const size_t numParts) {
+    std::vector<size_t> lengths;
+    for (size_t i = 0; i < numParts; i++) {
+        const auto cur = PRNG() % (left+1);
+        lengths.push_back(cur);
+        left -= cur;
+    }
+
+    std::vector<size_t> lengths_randomized;
+    for (size_t i = 0; i < numParts; i++) {
+        const auto cur = lengths.begin() + PRNG() % (lengths.size());
+        lengths_randomized.push_back(*cur);
+        lengths.erase(cur);
+    }
+
+    return lengths_randomized;
+}
+
 extern "C" size_t LLVMFuzzerMutate(uint8_t* data, size_t size, size_t maxSize);
 
 extern "C" size_t LLVMFuzzerCustomMutator(uint8_t* data, size_t size, size_t maxSize, unsigned int seed) {
     (void)seed;
-    if ( PRNG() % 2 == 0 ) {
+    if ( maxSize < 64 || PRNG() % 2 == 0 ) {
         goto end;
     }
-
     {
         const uint64_t operation = OperationLUT[ PRNG() % (sizeof(OperationLUT) / sizeof(OperationLUT[0])) ].id;
 
@@ -36,41 +53,75 @@ extern "C" size_t LLVMFuzzerCustomMutator(uint8_t* data, size_t size, size_t max
             case    CF_OPERATION("SymmetricEncrypt"):
                 {
                     nlohmann::json parameters;
-                    fuzzing::datasource::Datasource dsOut2(nullptr, 0);
-                    parameters["modifier"] = std::string((PRNG() % 1000) * 2, '0');
-                    parameters["cleartext"] = std::string((PRNG() % maxSize) * 2, '0');
+                    const bool aad_enabled = PRNG() % 2;
+                    const bool tagSize_enabled = PRNG() % 2;
+                    size_t numParts = 0;
+
+                    numParts++; /* modifier */
+                    numParts++; /* cleartext */
+                    numParts++; /* iv */
+                    numParts++; /* key */
+
+                    if ( aad_enabled ) {
+                        numParts++; /* aad */
+                    }
+
+                    auto lengths = SplitLength(maxSize - 64, numParts);
+
+                    parameters["modifier"] = std::string(lengths[0] * 2, '0');
+                    if ( PRNG() % 2 == 0 ) {
+                        if ( 16 < lengths[1] ) {
+                            lengths[1] = 16;
+                        }
+                    }
+                    parameters["cleartext"] = std::string(lengths[1] * 2, '0');
+
                     if ( PRNG() % 2 == 0 ) {
                         static const std::vector<size_t> ivSizes = {0, 1, 2, 4, 8, 12, 16, 24, 32, 64};
-                        const auto ivSize = ivSizes[PRNG() % ivSizes.size()];
-                        parameters["cipher"]["iv"] = std::string(ivSize * 2, '0');
-                    } else {
-                        parameters["cipher"]["iv"] = std::string((PRNG() % maxSize) * 2, '0');
+                        const auto newIvSize = ivSizes[PRNG() % ivSizes.size()];
+                        if ( newIvSize < lengths[2] ) {
+                            lengths[2] = newIvSize;
+                        }
                     }
+                    parameters["cipher"]["iv"] = std::string(lengths[2] * 2, '0');
+
                     if ( PRNG() % 2 == 0 ) {
                         static const std::vector<size_t> keySizes = {0, 1, 2, 4, 8, 12, 16, 24, 32, 64};
-                        const auto keySize = keySizes[PRNG() % keySizes.size()];
-                        parameters["cipher"]["key"] = std::string(keySize * 2, '0');
-                    } else {
-                        parameters["cipher"]["key"] = std::string((PRNG() % maxSize) * 2, '0');
+                        const auto newKeySize = keySizes[PRNG() % keySizes.size()];
+                        if ( newKeySize < lengths[3] ) {
+                            lengths[3] = newKeySize;
+                        }
                     }
-                    parameters["cipher"]["cipherType"] = CipherLUT[ PRNG() % (sizeof(CipherLUT) / sizeof(CipherLUT[0])) ].id;
-                    if ( PRNG() % 2 == 0 ) {
+                    parameters["cipher"]["key"] = std::string(lengths[3] * 2, '0');
+
+                    if ( aad_enabled ) {
                         parameters["aad_enabled"] = true;
-                        parameters["aad"] = std::string((PRNG() % maxSize) * 2, '0');
+                        if ( PRNG() % 2 == 0 ) {
+                            lengths[4] = 0;
+                        }
+                        parameters["aad"] = std::string(lengths[4] * 2, '0');
                     } else {
                         parameters["aad_enabled"] = false;
                     }
 
-                    parameters["ciphertextSize"] = PRNG();
-
-                    if ( PRNG() % 2 == 0 ) {
+                    if ( tagSize_enabled ) {
                         parameters["tagSize_enabled"] = true;
-                        parameters["tagSize"] = PRNG();
+                        if ( PRNG() % 2 == 0 ) {
+                            static const std::vector<size_t> tagSizes = {0, 1, 2, 4, 8, 12, 16, 24, 32, 64};
+                            const auto newTagSize = tagSizes[PRNG() % tagSizes.size()];
+                            parameters["tagSize"] = newTagSize;
+                        } else {
+                            parameters["tagSize"] = PRNG() % 102400;
+                        }
                     } else {
                         parameters["tagSize_enabled"] = false;
                     }
 
+                    parameters["cipher"]["cipherType"] = CipherLUT[ PRNG() % (sizeof(CipherLUT) / sizeof(CipherLUT[0])) ].id;
+                    parameters["ciphertextSize"] = PRNG() % (lengths[1] + 9);
+
                     cryptofuzz::operation::SymmetricEncrypt op(parameters);
+                    fuzzing::datasource::Datasource dsOut2(nullptr, 0);
                     op.Serialize(dsOut2);
                     const auto opSerialized = dsOut2.GetOut();
                     std::copy(opSerialized.begin(), opSerialized.end(), std::back_inserter(toAdd));
@@ -99,16 +150,13 @@ extern "C" size_t LLVMFuzzerCustomMutator(uint8_t* data, size_t size, size_t max
         dsOut.PutData(toAdd);
 
         /* Modifier */
-        dsOut.PutData(std::vector<uint8_t>(PRNG() % 256));
+        dsOut.PutData(std::vector<uint8_t>(0));
 
         /* Module ID */
         dsOut.Put<uint64_t>( ModuleLUT[ PRNG() % (sizeof(ModuleLUT) / sizeof(ModuleLUT[0])) ].id );
 
         /* Terminator */
         dsOut.Put<bool>(false);
-
-        fuzzing::datasource::Datasource dsOut2(nullptr, 0);
-        dsOut.PutData(std::vector<uint8_t>(PRNG() % maxSize));
 
         const auto insertSize = dsOut.GetOut().size();
         if ( insertSize <= maxSize ) {
