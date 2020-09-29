d4538d3b3e29637f0ec2c8f14e7e5c226141ecf6
Guido Vranken <guidovranken@gmail.com>
2020-09-29 18:08:34 +0200

    bignumber.js: Disable ExpMod due to timeouts

--- modules/bignumber.js/harness.js
+++ modules/bignumber.js/harness.js
@@ -37,10 +37,14 @@ var OpBignumCalc = function(FuzzerInput) {
     } else if ( IsNeg(calcOp) ) {
         ret = bn[0].negated();
     } else if ( IsExpMod(calcOp) ) {
+        /* Disabled due to timeouts */
+        return;
+        /*
         if ( bn[1].isGreaterThanOrEqualTo(new BigNumber("1000")) ) {
             return;
         }
         ret = bn[0].exponentiatedBy(bn[1]).modulo(bn[2]);
+        */
     } else if ( IsCmp(calcOp) ) {
         ret = new BigNumber( bn[0].comparedTo(bn[1]) );
     } else if ( IsAddMod(calcOp) ) {
