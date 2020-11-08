8fac2caf5f4d9a180f564f8f3be67c510c361b86
Guido Vranken <guidovranken@gmail.com>
2020-11-08 11:57:09 +0100

    libtomcrypt: Support SOSEMANUK

--- modules/libtomcrypt/module.cpp
+++ modules/libtomcrypt/module.cpp
@@ -898,6 +898,37 @@ end:
         return ret;
     }
 
+    std::optional<Buffer> SOSEMANUKCrypt(Datasource& ds, const Buffer& in, const component::SymmetricCipher& cipher) {
+        std::optional<Buffer> ret = std::nullopt;
+
+        if ( cipher.iv.GetSize() > 16 ) {
+            return ret;
+        }
+
+        if ( cipher.key.GetSize() == 0 || cipher.key.GetSize() > 32 ) {
+            return ret;
+        }
+
+        sosemanuk_state state;
+        size_t outIdx = 0;
+
+        uint8_t* out = util::malloc(in.GetSize());
+        const auto parts = util::ToParts(ds, in);
+
+        CF_CHECK_EQ(sosemanuk_setup(&state, cipher.key.GetPtr(), cipher.key.GetSize()), CRYPT_OK);
+        CF_CHECK_EQ(sosemanuk_setiv(&state, cipher.iv.GetPtr(), cipher.iv.GetSize()), CRYPT_OK);
+        for (const auto& part : parts) {
+            CF_CHECK_EQ(sosemanuk_crypt(&state, part.first, part.second, out + outIdx), CRYPT_OK);
+            outIdx += part.second;
+        }
+
+        ret = Buffer(out, in.GetSize());
+
+end:
+        util::free(out);
+        return ret;
+    }
+
     std::optional<Buffer> EcbEncrypt(operation::SymmetricEncrypt& op) {
         std::optional<Buffer> ret = std::nullopt;
 
@@ -1154,6 +1185,8 @@ std::optional<component::Ciphertext> libtomcrypt::OpSymmetricEncrypt(operation::
         return libtomcrypt_detail::ChaCha20Crypt(ds, op.cleartext, op.cipher);
     } else if ( op.cipher.cipherType.Get() == CF_CIPHER("RABBIT") ) {
         return libtomcrypt_detail::RABBITCrypt(ds, op.cleartext, op.cipher);
+    } else if ( op.cipher.cipherType.Get() == CF_CIPHER("SOSEMANUK") ) {
+        return libtomcrypt_detail::SOSEMANUKCrypt(ds, op.cleartext, op.cipher);
     } else if ( op.cipher.cipherType.Get() == CF_CIPHER("SALSA20_128") ) {
         return libtomcrypt_detail::Salsa20Crypt(ds, op.cleartext, op.cipher, 16, 20);
     } else if ( op.cipher.cipherType.Get() == CF_CIPHER("SALSA20_12_128") ) {
@@ -1188,6 +1221,8 @@ std::optional<component::Cleartext> libtomcrypt::OpSymmetricDecrypt(operation::S
         return libtomcrypt_detail::ChaCha20Crypt(ds, op.ciphertext, op.cipher);
     } else if ( op.cipher.cipherType.Get() == CF_CIPHER("RABBIT") ) {
         return libtomcrypt_detail::RABBITCrypt(ds, op.ciphertext, op.cipher);
+    } else if ( op.cipher.cipherType.Get() == CF_CIPHER("SOSEMANUK") ) {
+        return libtomcrypt_detail::SOSEMANUKCrypt(ds, op.ciphertext, op.cipher);
     } else if ( op.cipher.cipherType.Get() == CF_CIPHER("SALSA20_128") ) {
         return libtomcrypt_detail::Salsa20Crypt(ds, op.ciphertext, op.cipher, 16, 20);
     } else if ( op.cipher.cipherType.Get() == CF_CIPHER("SALSA20_12_128") ) {
