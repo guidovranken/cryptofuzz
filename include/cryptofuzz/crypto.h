8a40c2288d4dbd70f551a6d27fd80f5b1a440484
Guido Vranken <guidovranken@gmail.com>
2020-11-22 20:04:53 +0100

    Include SHA256 implementation

--- include/cryptofuzz/crypto.h
+++ include/cryptofuzz/crypto.h
@@ -9,5 +9,8 @@ namespace crypto {
 std::vector<uint8_t> sha1(const uint8_t* data, const size_t size);
 std::vector<uint8_t> sha1(const std::vector<uint8_t> data);
 
+std::vector<uint8_t> sha256(const uint8_t* data, const size_t size);
+std::vector<uint8_t> sha256(const std::vector<uint8_t> data);
+
 } /* namespace crypto */
 } /* namespace cryptofuzz */
