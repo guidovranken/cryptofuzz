73191756d9e7bcab838cf35cea88af8987cdc391
Guido Vranken <guidovranken@gmail.com>
2021-07-26 10:07:25 +0200

    Golang: Fix compilation warnings

--- modules/golang/generate_ids.cpp
+++ modules/golang/generate_ids.cpp
@@ -9,12 +9,12 @@ int main(void)
 {
     FILE* fp = fopen("ids.go", "wb");
     fprintf(fp, "package main\n");
-    for (const auto digest : DigestLUTMap ) {
+    for (const auto& digest : DigestLUTMap ) {
         auto digestStr = std::string(digest.second.name);
         digestStr = std::regex_replace(digestStr, std::regex("[.-]"), "_");
         fprintf(fp, "func is%s(id Type) bool { return id == %s }\n", digestStr.c_str(), std::to_string(digest.first).c_str());
     }
-    for (const auto cipher : CipherLUTMap ) {
+    for (const auto& cipher : CipherLUTMap ) {
         auto cipherStr = std::string(cipher.second.name);
         cipherStr = std::regex_replace(cipherStr, std::regex("[.-]"), "_");
         if ( cipherStr == "GOST_28147_89" ) {
@@ -23,12 +23,12 @@ int main(void)
         }
         fprintf(fp, "func is%s(id Type) bool { return id == %s }\n", cipherStr.c_str(), std::to_string(cipher.first).c_str());
     }
-    for (const auto curve : ECC_CurveLUTMap ) {
+    for (const auto& curve : ECC_CurveLUTMap ) {
         auto curveStr = std::string(curve.second.name);
         curveStr = std::regex_replace(curveStr, std::regex("[.-]"), "_");
         fprintf(fp, "func is%s(id Type) bool { return id == %s }\n", curveStr.c_str(), std::to_string(curve.first).c_str());
     }
-    for (const auto bnOp : CalcOpLUTMap ) {
+    for (const auto& bnOp : CalcOpLUTMap ) {
         auto bnOpStr = std::string(bnOp.second.name);
         bnOpStr = std::regex_replace(bnOpStr, std::regex("\\(.*"), "");
         fprintf(fp, "func is%s(id Type) bool { return id == %s }\n", bnOpStr.c_str(), std::to_string(bnOp.first).c_str());
