c01fc4fb87bb88084f40eeaa6117d4ac2f9d9588
Guido Vranken <guidovranken@gmail.com>
2021-11-29 14:22:51 +0100

    Tests: Verify ECDSA signature coordinates are less than curve order

--- tests.cpp
+++ tests.cpp
@@ -283,14 +283,31 @@ static void test_ECDSA_Signature(const uint64_t curveID, const std::string R, co
         return;
     }
 
-    boost::multiprecision::cpp_int r(R);
-    boost::multiprecision::cpp_int s(S);
+    const boost::multiprecision::cpp_int r(R), s(S);
+
     if ( r < 1 ) {
         std::cout << "ECDSA signature invalid: R < 1" << std::endl;
         ::abort();
     }
     if ( s < 1 ) {
         std::cout << "ECDSA signature invalid: S < 1" << std::endl;
+        ::abort();
+    }
+
+    const auto O = cryptofuzz::repository::ECC_CurveToOrder(curveID);
+    if ( O == std::nullopt ) {
+        return;
+    }
+
+    const boost::multiprecision::cpp_int o(*O);
+
+    if ( r >= o ) {
+        std::cout << "ECDSA signature invalid: R >= order" << std::endl;
+        ::abort();
+    }
+
+    if ( s >= o ) {
+        std::cout << "ECDSA signature invalid: S >= order" << std::endl;
         ::abort();
     }
 }
