d2b0b37b3fa716a554fa626b1943e2d438087142
Guido Vranken <guidovranken@gmail.com>
2022-01-07 06:45:42 +0100

    SolidityMath: Support Uniswap/solidity-lib sqrt

---
+++ modules/soliditymath/LOADER-github.com-Uniswap-solidity-lib-blob-master-contracts-libraries-Babylonian.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.0;
+
+import './github.com-Uniswap-solidity-lib-blob-master-contracts-libraries-Babylonian.sol';
+
+contract LOADER_github_com_Uniswap_solidity_lib_blob_master_contracts_libraries_Babylonian {
+    function sqrt(
+        uint256 a
+    ) public pure returns (uint256 result) {
+        return Babylonian.sqrt(a);
+    }
+}
