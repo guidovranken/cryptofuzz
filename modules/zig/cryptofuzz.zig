064fc9cae3eb78dd21923f56cff64f8ccd58afb6
Guido Vranken <guidovranken@gmail.com>
2022-11-05 02:20:45 +0100

    Zig: Fix build

--- modules/zig/cryptofuzz.zig
+++ modules/zig/cryptofuzz.zig
@@ -52,85 +52,87 @@ export fn cryptofuzz_zig_bignumcalc(
     b.setString(10, b_data[0..b_size]) catch unreachable;
 
     if ( op == 0 ) {
-        res.add(a.toConst(), b.toConst()) catch unreachable;
+        res.add(&a, &b) catch unreachable;
     } else if ( op == 1 ) {
-        res.sub(a.toConst(), b.toConst()) catch unreachable;
+        res.sub(&a, &b) catch unreachable;
     } else if ( op == 2 ) {
-        res.mul(a.toConst(), b.toConst()) catch unreachable;
+        res.mul(&a, &b) catch unreachable;
     } else if ( op == 3 ) {
-        if ( !a.toConst().positive ) {
+        if ( !a.isPositive() ) {
             return 1;
         }
-        if ( !b.toConst().positive ) {
+        if ( !b.isPositive()) {
             return 1;
         }
-        if ( b.toConst().eqZero() ) {
+        if ( b.eqZero() ) {
             return 1;
         }
 
         var mod = std.math.big.int.Managed.initSet(allocator, @as(usize, 1)) catch unreachable;
         defer mod.deinit();
 
-        res.divFloor(&mod, a.toConst(), b.toConst()) catch unreachable;
+        res.divFloor(&mod, &a, &b) catch unreachable;
     } else if ( op == 4 ) {
-        if ( !a.toConst().positive ) {
+        if ( !a.isPositive() ) {
             return 1;
         }
-        if ( !b.toConst().positive ) {
+        if ( !b.isPositive() ) {
             return 1;
         }
-        if ( a.toConst().eqZero() ) {
+        if ( a.eqZero() ) {
             return 1;
         }
-        if ( b.toConst().eqZero() ) {
+        if ( b.eqZero() ) {
             return 1;
         }
         //res.gcd(a, b) catch unreachable;
-        res.gcd(a, b) catch {
+        res.gcd(&a, &b) catch {
             return 1;
         };
     } else if ( op == 5) {
-        res.sqr(a.toConst()) catch unreachable;
+        res.sqr(&a) catch unreachable;
     } else if ( op == 6 ) {
-        if ( !a.toConst().positive ) {
+        if ( !a.isPositive() ) {
             return 1;
         }
-        if ( !b.toConst().positive ) {
+        if ( !b.isPositive() ) {
             return 1;
         }
-        if ( b.toConst().eqZero() ) {
+        if ( b.eqZero() ) {
             return 1;
         }
 
         var mod = std.math.big.int.Managed.initSet(allocator, @as(usize, 1)) catch unreachable;
         defer mod.deinit();
 
-        res.divFloor(&mod, a.toConst(), b.toConst()) catch unreachable;
+        res.divFloor(&mod, &a, &b) catch unreachable;
         res.swap(&mod);
     } else if ( op == 7 ) {
-        res.shiftLeft(a, 1) catch unreachable;
+        res.shiftLeft(&a, 1) catch unreachable;
     } else if ( op == 8 ) {
-        res.bitAnd(a, b) catch unreachable;
+        res.bitAnd(&a, &b) catch unreachable;
     } else if ( op == 9 ) {
-        res.bitOr(a, b) catch unreachable;
+        res.bitOr(&a, &b) catch unreachable;
     } else if ( op == 10 ) {
-        res.bitXor(a, b) catch unreachable;
+        res.bitXor(&a, &b) catch unreachable;
     } else if ( op == 11 ) {
-        res.copy(a.toConst().negate()) catch unreachable;
+        a.negate();
+        res.copy(a.toConst()) catch unreachable;
     } else if ( op == 12 ) {
-        res.copy(a.toConst().abs()) catch unreachable;
+        a.abs();
+        res.copy(a.toConst()) catch unreachable;
     } else if ( op == 13 ) {
-        res.set(a.toConst().bitCountAbs()) catch unreachable;
+        res.set(a.bitCountAbs()) catch unreachable;
     } else if ( op == 14 ) {
-        var count = b.toConst().to(usize) catch {
+        var count = b.to(usize) catch {
             return 1;
         };
-        res.shiftRight(a, count) catch unreachable;
+        res.shiftRight(&a, count) catch unreachable;
     } else if ( op == 15 ) {
-        var power = b.toConst().to(u32) catch {
+        var power = b.to(u32) catch {
             return 1;
         };
-        res.pow(a.toConst(), power) catch unreachable;
+        res.pow(&a, power) catch unreachable;
     } else {
         return 1;
     }
