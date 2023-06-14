d0652d680faf25ddb3a361c4e366288832e1ccaf
Guido Vranken <guidovranken@gmail.com>
2023-06-14 17:10:34 +0200

    wolfCrypt: Fix CondSet

--- modules/wolfcrypt/bn_ops.cpp
+++ modules/wolfcrypt/bn_ops.cpp
@@ -1175,8 +1175,7 @@ bool CondSet::Run(Datasource& ds, Bignum& res, BignumCluster& bn) const {
 
     const int doCopy = mp_iszero(bn[1].GetPtr()) ? 0 : 1;
     CF_CHECK_EQ(res.Set("0"), true);
-    MP_CHECK_EQ(mp_cond_copy(bn[0].GetPtr(), doCopy, bn.GetResPtr()), MP_OKAY);
-    CF_CHECK_TRUE(bn.CopyResult(res));
+    MP_CHECK_EQ(mp_cond_copy(bn[0].GetPtr(), doCopy, res.GetPtr()), MP_OKAY);
 
     ret = true;
 
