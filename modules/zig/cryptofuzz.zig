4d82666e1966091357d1972f898e1de78565209c
Guido Vranken <guidovranken@gmail.com>
2023-11-04 17:55:57 +0100

    Zig: Fix build

--- modules/zig/cryptofuzz.zig
+++ modules/zig/cryptofuzz.zig
@@ -831,21 +831,21 @@ export fn cryptofuzz_zig_ecc_validatepubkey(
         var a = P256.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         return 0;
     } else if ( curve == 1 ) {
         var a = Secp256k1.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         return 0;
     } else if ( curve == 2 ) {
         var a = P384.fromSerializedAffineCoordinates(
                 ax_data[0..48].*,
                 ay_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         return 0;
     } else {
@@ -859,15 +859,15 @@ export fn cryptofuzz_zig_ecc_privatetopublic(
         priv_bytes: [*:0]const u8,
         ) callconv(.C) i32 {
     if ( curve == 0 ) {
-        var res = P256.basePoint.mul(priv_bytes[0..32].*, .Big) catch return -1;
+        var res = P256.basePoint.mul(priv_bytes[0..32].*, .big) catch return -1;
         var resbytes = res.toUncompressedSec1();
         mem.copy(u8, r[0..65], &resbytes);
     } else if ( curve == 1 ) {
-        var res = Secp256k1.basePoint.mul(priv_bytes[0..32].*, .Big) catch return -1;
+        var res = Secp256k1.basePoint.mul(priv_bytes[0..32].*, .big) catch return -1;
         var resbytes = res.toUncompressedSec1();
         mem.copy(u8, r[0..65], &resbytes);
     } else if ( curve == 2 ) {
-        var res = P384.basePoint.mul(priv_bytes[0..48].*, .Big) catch return -1;
+        var res = P384.basePoint.mul(priv_bytes[0..48].*, .big) catch return -1;
         var resbytes = res.toUncompressedSec1();
         mem.copy(u8, r[0..97], &resbytes);
     }
@@ -887,11 +887,11 @@ export fn cryptofuzz_zig_ecc_point_add(
         var a = P256.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var b = P256.fromSerializedAffineCoordinates(
                 bx_data[0..32].*,
                 by_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.add(b);
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         if ( b.rejectIdentity() == error.IdentityElement ) return -1;
@@ -901,11 +901,11 @@ export fn cryptofuzz_zig_ecc_point_add(
         var a = Secp256k1.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var b = Secp256k1.fromSerializedAffineCoordinates(
                 bx_data[0..32].*,
                 by_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.add(b);
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         if ( b.rejectIdentity() == error.IdentityElement ) return -1;
@@ -915,11 +915,11 @@ export fn cryptofuzz_zig_ecc_point_add(
         var a = P384.fromSerializedAffineCoordinates(
                 ax_data[0..48].*,
                 ay_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var b = P384.fromSerializedAffineCoordinates(
                 bx_data[0..48].*,
                 by_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.add(b);
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         if ( b.rejectIdentity() == error.IdentityElement ) return -1;
@@ -943,11 +943,11 @@ export fn cryptofuzz_zig_ecc_point_sub(
         var a = P256.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var b = P256.fromSerializedAffineCoordinates(
                 bx_data[0..32].*,
                 by_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.sub(b);
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         if ( b.rejectIdentity() == error.IdentityElement ) return -1;
@@ -957,11 +957,11 @@ export fn cryptofuzz_zig_ecc_point_sub(
         var a = Secp256k1.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var b = Secp256k1.fromSerializedAffineCoordinates(
                 bx_data[0..32].*,
                 by_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.sub(b);
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         if ( b.rejectIdentity() == error.IdentityElement ) return -1;
@@ -971,11 +971,11 @@ export fn cryptofuzz_zig_ecc_point_sub(
         var a = P384.fromSerializedAffineCoordinates(
                 ax_data[0..48].*,
                 ay_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var b = P384.fromSerializedAffineCoordinates(
                 bx_data[0..48].*,
                 by_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.sub(b);
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         if ( b.rejectIdentity() == error.IdentityElement ) return -1;
@@ -999,13 +999,13 @@ export fn cryptofuzz_zig_ecc_point_mul(
         var a = P256.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         if ( alt == 0 ) {
-            var res = a.mulPublic(b_data[0..32].*, .Big) catch return -1;
+            var res = a.mulPublic(b_data[0..32].*, .big) catch return -1;
             var resbytes = res.toUncompressedSec1();
             mem.copy(u8, r[0..65], &resbytes);
         } else {
-            var res = a.mul(b_data[0..32].*, .Big) catch return -1;
+            var res = a.mul(b_data[0..32].*, .big) catch return -1;
             var resbytes = res.toUncompressedSec1();
             mem.copy(u8, r[0..65], &resbytes);
         }
@@ -1013,13 +1013,13 @@ export fn cryptofuzz_zig_ecc_point_mul(
         var a = Secp256k1.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         if ( alt == 0 ) {
-            var res = a.mulPublic(b_data[0..32].*, .Big) catch return -1;
+            var res = a.mulPublic(b_data[0..32].*, .big) catch return -1;
             var resbytes = res.toUncompressedSec1();
             mem.copy(u8, r[0..65], &resbytes);
         } else {
-            var res = a.mul(b_data[0..32].*, .Big) catch return -1;
+            var res = a.mul(b_data[0..32].*, .big) catch return -1;
             var resbytes = res.toUncompressedSec1();
             mem.copy(u8, r[0..65], &resbytes);
         }
@@ -1027,13 +1027,13 @@ export fn cryptofuzz_zig_ecc_point_mul(
         var a = P384.fromSerializedAffineCoordinates(
                 ax_data[0..48].*,
                 ay_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         if ( alt == 0 ) {
-            var res = a.mulPublic(b_data[0..48].*, .Big) catch return -1;
+            var res = a.mulPublic(b_data[0..48].*, .big) catch return -1;
             var resbytes = res.toUncompressedSec1();
             mem.copy(u8, r[0..97], &resbytes);
         } else {
-            var res = a.mul(b_data[0..48].*, .Big) catch return -1;
+            var res = a.mul(b_data[0..48].*, .big) catch return -1;
             var resbytes = res.toUncompressedSec1();
             mem.copy(u8, r[0..97], &resbytes);
         }
@@ -1053,7 +1053,7 @@ export fn cryptofuzz_zig_ecc_point_neg(
         var a = P256.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.neg();
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         var resbytes = res.toUncompressedSec1();
@@ -1062,7 +1062,7 @@ export fn cryptofuzz_zig_ecc_point_neg(
         var a = Secp256k1.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.neg();
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         var resbytes = res.toUncompressedSec1();
@@ -1071,7 +1071,7 @@ export fn cryptofuzz_zig_ecc_point_neg(
         var a = P384.fromSerializedAffineCoordinates(
                 ax_data[0..48].*,
                 ay_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.neg();
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         var resbytes = res.toUncompressedSec1();
@@ -1092,7 +1092,7 @@ export fn cryptofuzz_zig_ecc_point_dbl(
         var a = P256.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.dbl();
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         var resbytes = res.toUncompressedSec1();
@@ -1101,7 +1101,7 @@ export fn cryptofuzz_zig_ecc_point_dbl(
         var a = Secp256k1.fromSerializedAffineCoordinates(
                 ax_data[0..32].*,
                 ay_data[0..32].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.dbl();
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         var resbytes = res.toUncompressedSec1();
@@ -1110,7 +1110,7 @@ export fn cryptofuzz_zig_ecc_point_dbl(
         var a = P384.fromSerializedAffineCoordinates(
                 ax_data[0..48].*,
                 ay_data[0..48].*,
-                .Big) catch return -1;
+                .big) catch return -1;
         var res = a.dbl();
         if ( a.rejectIdentity() == error.IdentityElement ) return -1;
         var resbytes = res.toUncompressedSec1();
